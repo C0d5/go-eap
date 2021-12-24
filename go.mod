033878ca28a35ef5d642b944b9d94e5a1e10774c
sonam pundir <gusain.sonam@hotmail.com>
2021-12-24 23:14:47 +0530

    tls code added

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/C0d5/go-eap
+
+go 1.17
+
+require (
+	github.com/C0d5/go-tls v0.0.0-20211224171801-9084ac1f0694
+	layeh.com/radius v0.0.0-20210819152912-ad72663a72ab
+)
+
+require (
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
---
+++ producer.go
@@ -0,0 +1,29 @@
+package eap
+
+import(
+	"layeh.com/radius"
+	"layeh.com/radius/rfc2865"
+	"layeh.com/radius/rfc2869"
+	tlsclient "github.com/C0d5/go-tls/core"
+	model "github.com/C0d5/go-tls/model"
+)
+
+func TLSHello() []byte {
+	client := tlsclient.MakeTLSClient()
+	clientHello := model.MakeClientHello(client.tlsVersion)
+	clientHelloPayload := clientHello.GetClientHelloPayload()
+	eap := &TLSPacket{
+		PacketHeader: &PacketHeader{
+			Outer : &{
+				code : EAPRequest,
+				id : 13,
+				msgType: TLS
+			},
+			Flags : FlagStart,
+			Lengthv: len(clientHelloPayload)
+		}
+		Data : clientHelloPayload
+	}
+	_,buf := eap.Encode()
+	print(buf)
+}
